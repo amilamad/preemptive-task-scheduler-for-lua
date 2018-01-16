086b1ae27432f8ed6a1d5a8a1b50551601a1f47d
amilamad <amilamad@gmail.com>
2018-01-16 15:57:35 +0530

    Update non_blocking_test.lua
    Cleanup

--- non_blocking_test.lua
+++ non_blocking_test.lua
@@ -1,9 +1,7 @@
 function heavy_function(dt)
-	print("onSimulationStep ", dt)  
-			
 	i = 0;
 	while i < 80000 do
 	  i = i + 1;
-      --print("lua while ", i)  	
-    end
+	  --print("lua while ", i)  	
+    	end
 end
